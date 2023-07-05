f53917561936b21d0e0be5cbacc1ab4fb41abbe9
Jason Wong <wongjason415@gmail.com>
2023-07-05 15:34:45 -0700

    initial client commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import UploadButton from "./components/uploadButton";
+
+function App() {
+  return (
+    <div className="App">
+      <UploadButton />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/uploadButton.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+const UploadButton = () => {
+  const [selectedFile, setSelectedFile] = useState();
+  const [isFilePicked, setIsFilePicked] = useState(false);
+
+  const changeHandler = (event) => {
+    setSelectedFile(event.target.files[0]);
+
+    setIsFilePicked(true);
+  };
+
+  const handleSubmission = () => {};
+  return (
+    <div>
+      <input type="file" name="file" onChange={changeHandler} />
+
+      <div>
+        <button onClick={handleSubmission}>Submit</button>
+      </div>
+    </div>
+  );
+};
+
+export default UploadButton;
